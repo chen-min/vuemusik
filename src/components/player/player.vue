c0d38c98a955e6e82cc3acd171a7e8e6df5b1ca0
chenm <chenmin@hotmail.de>
2018-06-28 16:32:11 +0800

    add player

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="player" v-show="playlist.length > 0">
     <transition>
-      <div class="normal-player">
+      <div class="normal-player" v-show="fullScreen">
         <div class="background">
-          <img src="" alt="">
+          <img width="100%" height="100%" :src="currentSong.image" >
         </div>
         <div class="top">
-          <div class="back">
+          <div class="back" @click="back">
             <i class="icon-back"></i>
           </div>
-            <h1 class="title"></h1>
-            <h1 class="subtitile"></h1>
+            <h1 class="title" v-html="currentSong.name"></h1>
+            <h1 class="subtitle" v-html="currentSong.singer"></h1>
         </div>
           <!--旋转的唱片-->      
         <div class="middle">
-          <div class="middle-l">
-            <div class="cd">
-              <img src="" alt="" class="imagje">
+          <!-- 旋转的唱片 -->
+          <div class="middle-l" ref="middleL">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class='cd' :class="cdCls">
+                <img :src="currentSong.image" alt="" class="image">
+              </div>
             </div>
           </div>
   <!--         <div class="playing-lyric-wrapper">
@@ -34,39 +37,53 @@
         <!-- 底部操作区域 -->
         <div class="bottom">
           <div class="dot-wrapper">
-            <span class="dot"></span>
-            <span class="dot"></span>
+            <span class="dot" :class="{active: currentShow == 'cd'}"></span>
+            <span class="dot" :class="{active: currentShow == 'lyric'}"></span>
           </div>
           <div class="progress-wrapper">
-            <span class="time time-l"></span>
+            <span class="time time-l"> {{format(currentTime)}} </span>
             <div class="progress-bar-wrapper">
               
             </div>
-            <span class="time time-r"></span>
+            <span class="time time-r"> {{ format(currentSong.duration) }} </span>
           </div>
           <div class="operators">
             <div class="icon icon-left">
-              <i class="modeIcon"></i>
+              <i :class="modeIcon"></i>
             </div>
-            <div class="icon icon-left">
-              <i class="modeIcon"></i>
+            <div class="icon icon-left" :class='disableCls'>
+              <i class="icon-prev"></i>
             </div>
-            <div class="icon icon-left">
-              <i class="modeIcon"></i>
+            <div class="icon icon-center" :class='disableCls'>
+              <i :class="playIcon"></i>
             </div>
-            <div class="icon icon-left">
-              <i class="modeIcon"></i>
+            <div class="icon icon-right" :class='disableCls'>
+              <i class="icon-next"></i>
             </div>
-            <div class="icon icon-left">
-              <i class="modeIcon"></i>
+            <div class="icon icon-right">
+              <i class="icon icon-not-favorite"></i>
             </div>
           </div>
         </div>
       </div>
     </transition>
-
+     
     <transition>
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img width="40" height="40" :src="currentSong.image" :class="cdCls">
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control">
 
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
+        </div>
+      </div>
     </transition>
   </div>
 </template>
@@ -74,21 +91,76 @@
 <script>
 import {mapGetters, mapMutations} from 'vuex'
 import Scroll from 'base/scroll/scroll'
-
+import { playMode } from 'common/js/config'
 export default {
   
   components: {
-  },
 
+  },
+  data() {
+    return {
+      songReady:false,
+      currentTime: 0,
+      currentLyric: null,
+      currentLineNum: 0,
+      currentShow: 'cd',
+      playingLyric: ''
+    }
+  },
   computed: {
+    playIcon () {
+      return this.playing ? 'icon-pause' : 'icon-play-mini'
+    },
+    miniIcon () {
+      return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+    },
+    modeIcon () {
+      switch (this.mode) {
+        case playMode.random: return 'icon-random'
+        case playMode.loop: return 'icon-loop'
+        case playMode.sequence:
+        default: return 'icon-sequence'
+      }
+    },
+    cdCls () {
+      return this.playing ? 'play' : 'play pause'
+    },
+    disableCls () {
+      return this.songReady ? '' : 'disable'
+    },
+    percent () {
+      return this.currentTime / this.currentSong.duration
+    },
     ...mapGetters([
         'playlist',
+        'fullScreen',
+        'currentSong'
     ]),
   },
+
   methods: {
+    back () {
+      this.setFullScreen(false)
+    },
+    open () {
+      this.setFullScreen(true)
+    },
+    ready () {
+      this.songReady = true
+    },
+    error () {
+      this.songReady = true
+    },
+
     ...mapMutations({
     }),
-  },
+    format() {
+
+    },
+    ...mapMutations({
+      setFullScreen: 'SET_FULLSCREEN',
+    })
+  }
   
 
 }
@@ -96,12 +168,238 @@ export default {
 
 <style lang="stylus">
 .player
+  .normal-player
     position: fixed
     left: 0
     right: 0
     top: 0
     bottom: 0
-    z-index: 100
-
-
+    z-index: 150
+    background: $color-background
+    .background
+      position: absolute
+      left: 0
+      top: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      opacity: 0.6
+      filter: blur(20px)
+    .top
+      position: relative
+      margin-bottom: 25px
+      .back
+        position: absolute
+        top: 0
+        left: 6px
+        z-index: 50
+        .icon-back
+          display: inline-block
+          padding: 9px
+          font-size: $font-size-large-x
+          color: $color-theme
+          transform: rotate(-90deg)
+      .title
+        width: 70%
+        margin: 0 auto
+        line-height: 40px
+        text-align: center
+        no-wrap()
+        font-size: $font-size-large
+        color: $color-text
+      .subtitle
+        line-height: 20px
+        text-align: center
+        font-size: $font-size-medium
+        color: $color-text
+    .middle
+      position: fixed
+      width: 100%
+      top: 80px
+      bottom: 170px
+      white-space: nowrap
+      font-size: 0
+      .middle-l
+        display: inline-block
+        vertical-align: top
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 80%
+        .cd-wrapper
+          position: absolute
+          left: 10%
+          top: 0
+          width: 80%
+          height: 100%
+          .cd
+            width: 100%
+            height: 100%
+            // 歌曲图片旋转
+            &.play
+              animation rotate 20s linear infinite
+            &.pause
+              animation-play-state paused
+            .image
+              position: absolute
+              left: 0
+              top: 0
+              width: 100%
+              height: 100%
+              box-sizing: border-box
+              border-radius: 50%
+              border: 10px solid rgba(255, 255, 255, 0.1)
+        .playing-lyric-wrapper
+          width: 80%
+          margin: 30px auto 0 auto
+          overflow: hidden
+          text-align: center
+          .playing-lyric
+            height: 20px
+            line-height: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+      .middle-r
+        display: inline-block
+        vertical-align: top
+        width: 100%
+        height: 100%
+        overflow: hidden
+        .lyric-wrapper
+          width: 80%
+          margin: 0 auto
+          overflow: hidden
+          text-align: center
+          .text
+            line-height: 32px
+            color: $color-text-l
+            font-size: $font-size-medium
+            &.current
+              color: $color-text
+    .bottom
+      position: absolute
+      bottom: 50px
+      width: 100%
+      .dot-wrapper
+        text-align: center
+        font-size: 0
+        .dot
+          display: inline-block
+          vertical-align: middle
+          margin: 0 4px
+          width: 8px
+          height: 8px
+          border-radius: 50%
+          background: $color-text-l
+          &.active
+            width: 20px
+            border-radius: 5px
+            background: $color-text-ll
+      .progress-wrapper
+        display: flex
+        align-items: center
+        width: 80%
+        margin: 0px auto
+        padding: 10px 0
+        .time
+          color: $color-text
+          font-size: $font-size-small
+          flex: 0 0 30px
+          line-height: 30px
+          width: 30px
+          &.time-l
+            text-align: left
+          &.time-r
+            text-align: right
+        .progress-bar-wrapper
+          flex: 1
+      .operators
+        display: flex
+        align-items: center
+        .icon
+          flex: 1
+          color: $color-theme
+          &.disable
+            color: $color-theme-d
+          i
+            font-size: 30px
+        .icon-left
+          text-align: right
+        .icon-center
+          padding: 0 20px
+          text-align: center
+          i
+            font-size: 40px
+        .icon-right
+          text-align: left
+    // 过渡
+    &.normal-enter-active, &.normal-leave-active
+      transition: all 0.4s
+      .top, .bottom
+        transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+    &.normal-enter, &.normal-leave-to
+      opacity: 0
+      .top
+        transform: translate3d(0, -100px, 0)
+      .bottom
+        transform: translate3d(0, 100px, 0)
+  // 迷你播放器
+  .mini-player
+    display: flex
+    align-items: center
+    position: fixed
+    left: 0
+    bottom: 0
+    z-index: 180
+    width: 100%
+    height: 60px
+    background: $color-highlight-background
+    &.mini-enter-active, &.mini-leave-active
+      transition: all 0.4s
+    &.mini-enter, &.mini-leave-to
+      opacity: 0
+    .icon
+      flex: 0 0 40px
+      width: 40px
+      padding: 0 10px 0 20px
+      img
+        border-radius: 50%
+        &.play
+          animation: rotate 10s linear infinite
+        &.pause
+          animation-play-state: paused
+    .text
+      display: flex
+      flex-direction: column
+      justify-content: center
+      flex: 1
+      line-height: 20px
+      overflow: hidden
+      .name
+        margin-bottom: 2px
+        no-wrap()
+        font-size: $font-size-medium
+        color: $color-text-d
+      .desc
+        no-wrap()
+        font-size: $font-size-small
+        color: $color-text-d
+    .control
+      flex: 0 0 30px
+      width: 30px
+      padding: 0 10px
+      .icon-playlist, .icon-play-mini, .icon-pause-mini
+        font-size: 30px
+        color: $color-theme-d
+      .icon-mini
+        font-size: 32px
+        position: absolute
+        left: 0
+        top: 0
+// 旋转动画
+@keyframes rotate
+  0%
+    transform: rotate(0)
+  100%
+    transform: rotate(360deg)
 </style>
